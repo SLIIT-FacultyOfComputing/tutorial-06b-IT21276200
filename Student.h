42459edb1bdbbc3cfa3a53e8ba86b11267521b6e
IT21276200 <89300501+IT21276200@users.noreply.github.com>
2022-03-16 05:41:56 +0000

    complete

--- Student.h
+++ Student.h
@@ -1,9 +1,16 @@
-class Student {
+class Student
+{
   // private section
+  private:
   //    int studentId
+        int studentId;
   //    name <- 20 charcters
+        char name[20];
   
   // public section
+  public;
   //    assignDetails() method declaration
+       void assingnDetails(int psudentId,char pname[20]);
   //    display() method declaration
+       void display();
 };
